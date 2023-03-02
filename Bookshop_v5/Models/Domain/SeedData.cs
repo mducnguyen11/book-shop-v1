6092b93f35d3afb71c1a786eae175988119527fa
mducnguyen11 <mducnguyen11@gmail.com>
2023-03-02 21:33:36 +0700

    Data seeding for Genre

---
+++ Bookshop_v5/Models/Domain/SeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Bookshop_v5.Models.Domain
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new DatabaseContext(
+                serviceProvider.GetRequiredService<DbContextOptions<DatabaseContext>>()))
+            {
+                context.Database.EnsureCreated();
+
+                // Tạo data seeding cho các class khác ở đây
+
+                InitializeGenres(context);
+            }
+        }
+
+        private static void InitializeGenres(DatabaseContext context)
+        {
+            if (!context.Genre.Any())
+            {
+                var genres = new Genre[]
+                {
+            new Genre{Name="Fantasy"},
+            new Genre{Name="Science Fiction"},
+            new Genre{Name="Mystery"},
+            new Genre{Name="Romance"},
+            new Genre{Name="Horror"},
+            new Genre{Name="Thriller"},
+            new Genre{Name="Historical Fiction"},
+            new Genre{Name="Young Adult"},
+            new Genre{Name="Children"},
+                };
+
+                context.Genre.AddRange(genres);
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
--- Readme.md
+++ Readme.md
@@ -1,4 +1,5 @@
 ASP.net core project !!!
 Cách chạy database :
 mở nuget package console
+chạy lệnh : add-migration
 chạy lệnh : update-database
